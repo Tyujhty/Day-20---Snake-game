e8c51a2cf4eb63ce84c7936234657bd802593349
Tyujhty <fannybaquey@gmail.com>
2024-01-19 16:18:54 +0100

    feat: Create snake object. Add two functions

--- snake.py
+++ snake.py
@@ -1,6 +1,7 @@
 from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
 
 
 class Snake:
@@ -24,4 +25,4 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(20)
+        self.segments[0].forward(MOVE_DISTANCE)
